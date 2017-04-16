17ccade6aaed0dea911aa159c19712dd05358ac3
Łukasz Kobylański <lukaskobylanski@gmail.com>
2017-04-16 22:20:50 +0200

    Money-Changer v2.0
    Fixed an issue where the user could provide a data different from PL nomilans but still within 2zl - 200zl like 45 zl. Now application throw the message, that user should put in a money of exact value: 200, 100, 50, 20, 10, 5, or 2 PLN. All comments are included within the file in the code.

--- change.php
+++ change.php
@@ -16,26 +16,37 @@ public function displayData() //metoda wyswietli mozliwe kombinacje
 		{
 			echo "Proszę podać wartość nominału za pomocą cyfr";
 		}
-		else if($this->money > 200 || $this->money < 2) //walidacja dla nominału
+		else if(is_numeric($this->money) == true)
 		{
-			echo "Proszę podać nominał w przedziale 200zł - 2zł";
-		}
-		else
-			{		
-			foreach($nomi as $value) //dla kazdego nominalu z przedzialu $nomi zapisanego jako $value
+			$counter = 0;
+			foreach($nomi as $value)
 			{
-				if($this->money > $value) //jezeli dane wpisane przez usytkownika sa wieksze niz dany element ($value) szeregu $nomi
+				if($this->money == $value)
 				{
-					$result = ($this->money / $value); //do zmiennej $result dodajemy wynik dzielenia liczby usera przez $value to nam da ilosc potrzebnych banktonow lub monet o nomilane $value
-					
-				if($this->money % $value != 0) //jezeli reszta z dzielenia != 0 jest to mozliwe tylko w 2 przypadkach 50zl i 5zl
+					$counter++;
+				}
+			}
+			if($counter == 0)
+			{
+				echo "Wprowadz pieniadz: 200, 100, 50, 20, 10, 5, 2 zl";
+			}
+			else
+				{		
+				foreach($nomi as $value) //dla kazdego nominalu z przedzialu $nomi zapisanego jako $value
 				{
-					$rest = ($this->money % $value); //jest to reszta z tego dzielenia                       //$rest banknot o nizszym nominale niż $value
-					echo "Mozna rozmienic na: " . (($this->money - $rest)/$value) . " po " . $value . "zł oraz " . $rest . "zł<br><br>";
-				}   //powyzsze dzialanie pozwala nam wyliczyc bez reszty ile dostaniemy banknotow po np. 20zl. Odejmujemy $rest do money poniewaz kwota podana przez uzytkownika jest stala np. 50 i to od niej musimy odjac reszte z dzielenia by otrzymac wynik bez reszty 
-					else
+					if($this->money > $value) //jezeli dane wpisane przez usytkownika sa wieksze niz dany element ($value) szeregu $nomi
 					{
-						echo "Mozna rozmienic na: " . $result . " po " . $value . "zł <br><br>"; //gdy % == 0 od razu przechodzimy do wyniku
+						$result = ($this->money / $value); //do zmiennej $result dodajemy wynik dzielenia liczby usera przez $value to nam da ilosc potrzebnych banktonow lub monet o nomilane $value
+						
+					if($this->money % $value != 0) //jezeli reszta z dzielenia != 0 jest to mozliwe tylko w 2 przypadkach 50zl i 5zl
+					{
+						$rest = ($this->money % $value); //jest to reszta z tego dzielenia                       //$rest banknot o nizszym nominale niż $value
+						echo "Mozna rozmienic na: " . (($this->money - $rest)/$value) . " po " . $value . "zł oraz " . $rest . "zł<br><br>";
+					}   //powyzsze dzialanie pozwala nam wyliczyc bez reszty ile dostaniemy banknotow po np. 20zl. Odejmujemy $rest do money poniewaz kwota podana przez uzytkownika jest stala np. 50 i to od niej musimy odjac reszte z dzielenia by otrzymac wynik bez reszty 
+						else
+						{
+							echo "Mozna rozmienic na: " . $result . " po " . $value . "zł <br><br>"; //gdy % == 0 od razu przechodzimy do wyniku
+						}
 					}
 				}
 			}
